aa68bb19f3acffd9190878f210824fa885eb3215
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-03-23 20:01:49 -0400

    Parse status JSON response

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
 extern crate num_cpus;
+extern crate serde_json;
 extern crate tokio_core;
 
 pub mod uli;
--- src/status.rs
+++ src/status.rs
@@ -2,14 +2,16 @@ extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
 extern crate num_cpus;
+extern crate serde_json;
 extern crate tokio_core;
 
-use std::io::{self, Write};
-use hyper::Client;
+use std::io;
+use hyper::{Client, Error};
 use hyper_tls::HttpsConnector;
 use tokio_core::reactor::Core;
 use futures::future::Future;
 use futures::Stream;
+use serde_json::Value;
 
 pub fn hmda_api_status(url: &str) -> Result<(), String> {
     let mut core = Core::new().unwrap();
@@ -21,11 +23,21 @@ pub fn hmda_api_status(url: &str) -> Result<(), String> {
 
     let uri = url.parse().unwrap();
 
-    let work = client.get(uri).and_then(|res| {
-        res.body()
-            .for_each(|chunk| io::stdout().write_all(&chunk).map_err(From::from))
+    let work_verbose = client.get(uri).and_then(|res| {
+        res.body().concat2().and_then(move |body| {
+            let v: Value =
+                serde_json::from_slice(&body).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            println!("Status for service {} is {}", v["service"], v["status"]);
+            Ok(())
+        })
     });
-    Ok(core.run(work).unwrap())
+
+    //let work = client.get(uri).and_then(|res| {
+    //    res.body()
+    //        .for_each(|chunk| io::stdout().write_all(&chunk).map_err(From::from))
+    //});
+    core.run(work_verbose).unwrap();
+    Ok(())
 }
 
 #[cfg(test)]
